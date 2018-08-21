879aced8d040b559ad4bfb3b8052faec14f7929a
KevinvanHeezik <0958302@hr.nl>
2018-08-21 13:15:25 +0200

    Alle data wordt aan de student gevraagd en weg geschreven bij de juiste student.

--- src/formulier_studenten/Formulier_Studenten.java
+++ src/formulier_studenten/Formulier_Studenten.java
@@ -1,6 +1,7 @@
 package formulier_studenten;
 
 import java.sql.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Formulier_Studenten {
@@ -10,30 +11,50 @@ public class Formulier_Studenten {
     public static void main(String[] args) {
 
         Formulier_Studenten formulier = new Formulier_Studenten();
-        
+
         formulier.getConnection();
     }
-    
-    public static String getID(){
-        
+
+    public static String getID() {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Student nummer");
         String id = scanner.nextLine();
         System.out.println("Student nummer is " + id);
         return id;
     }
-    
-    public static String getMedestudent(){
+
+    public static String getVooropleiding() {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Vooropleiding");
+        String vooropleiding = scanner.nextLine();
+        System.out.println("Vooropleiding is " + vooropleiding);
+        return vooropleiding;
+    }
+
+    public static String getGebdatum() {
         
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Geboortedatum");
+        String gebdatum = scanner.next();
+        System.out.println("Geboortedatum is " + gebdatum);
+        return gebdatum;
+
+        }
+
+
+    public static String getMedestudent() {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Medestudent");
         String medestudent = scanner.nextLine();
         System.out.println("Medestudent is " + medestudent);
         return medestudent;
-    } 
-    
-    public static String getOverig(){
-        
+    }
+
+    public static String getOverig() {
+
         Scanner scanner = new Scanner(System.in);
         System.out.println("Overig");
         String overig = scanner.nextLine();
@@ -51,13 +72,20 @@ public class Formulier_Studenten {
 
             conn = DriverManager.getConnection(url, username, password);
             System.out.println("Connected");
-            
+
             Student student = new Student();
-            
+
             student.id = getID();
+            student.vooropleiding = getVooropleiding();
+
+            String datum = getGebdatum();
+            java.util.Date utilDate = new SimpleDateFormat("yyyy-MM-dd").parse(datum);
+            java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
+            student.gebdatum = sqlDate;
+
             student.medestudent = getMedestudent();
             student.overig = getOverig();
-            
+
             student.update(conn);
 
         } catch (Exception e) {
--- src/formulier_studenten/Student.java
+++ src/formulier_studenten/Student.java
@@ -7,22 +7,27 @@ public class Student {
     public static String COMMA = "; ";
 
     String id;
+    String vooropleiding;
+    Date gebdatum;
     String medestudent;
     String overig;
 
     void update(Connection conn) throws Exception {
-        String query = "UPDATE klassenindeling.studenten SET Medestudent = ?, Overig = ? WHERE Studenten_Nummer = ?;";
+        String query = "UPDATE klassenindeling.studenten SET Medestudent = ?, Overig = ?, Vooropleiding = ?, Geboortedatum = ? WHERE Studenten_Nummer = ?;";
 
         PreparedStatement preparedStmt = conn.prepareStatement(query);
-        preparedStmt.setString(3, id);
-        preparedStmt.setString(2, overig);
         preparedStmt.setString(1, medestudent);
+        preparedStmt.setString(3, vooropleiding);
+        preparedStmt.setDate(4, gebdatum);
+        preparedStmt.setString(5, id);
+        preparedStmt.setString(2, overig);
+        
 
         preparedStmt.executeUpdate();
     }
 
-    @Override
+    /*@Override
     public String toString() {
         return id + COMMA + overig + COMMA + medestudent;
-    }
+    }*/
 }
